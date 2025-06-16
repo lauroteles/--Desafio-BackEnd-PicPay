a5e1a4e53e59efb236185fec3a244621caabb457
lauroteles <laurotfl@gmail.com>
2025-06-16 18:06:17 -0300

    Criando repositories

--- picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.picpaysimplificado.repositories;
 
-import com.picpaysimplificado.domain.user.Transaction;
+import com.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
