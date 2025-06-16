795f47d33d06d9be08844c6d75f8a9be3f6e590c
lauroteles <laurotfl@gmail.com>
2025-06-16 18:05:38 -0300

    Criando Controllers

--- picpaysimplificado/src/main/java/com/picpaysimplificado/controller/TransactionController.java
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/controller/TransactionController.java
@@ -1,4 +1,26 @@
 package com.picpaysimplificado.controller;
 
+import com.picpaysimplificado.DTO.TransacationDTO;
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransacationDTO) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return  new ResponseEntity<>(Transaction, HttpStatus.OK);
+    }
+
 }
